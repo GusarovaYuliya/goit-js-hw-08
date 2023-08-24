e3ed1046cc6d0a9eeeedcbee5084adc9d8bc3810
GusarovaYuliya <gusarovayuliya25@gmail.com>
2023-08-24 17:18:41 +0300

    comm

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,6 +3,7 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
+console.log(galleryItems);
 
 const images = document.querySelectorAll(".lightbox-image");
 const lightbox = new SimpleLightbox(".lightbox-image");
