f10dde1c9bc437981159469a6df61bbcae1b9f27
GusarovaYuliya <93098363+GusarovaYuliya@users.noreply.github.com>
2023-08-24 16:19:41 +0300

    comm

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,14 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+const images = document.querySelectorAll(".lightbox-image");
+const lightbox = new SimpleLightbox(".lightbox-image");
+
+images.forEach((image) => {
+    image.addEventListener("click", () => {
+      lightbox.open();
+    });
+  });
