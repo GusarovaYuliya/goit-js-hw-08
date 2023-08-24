534209687525602402f56ef7eb26212854ab913d
GusarovaYuliya <93098363+GusarovaYuliya@users.noreply.github.com>
2023-08-24 22:44:46 +0300

    commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -5,3 +5,25 @@ import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
 console.log(galleryItems);
+
+const listEl = document.querySelector(".gallery");
+const renderList = galleryItems
+.map(({ preview, original, description }) => {
+  return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+  <img
+  class="gallery__image"
+  src="${preview}"
+  alt="${description}"
+  />
+  </a>
+  </li>`;
+})
+.join("");
+
+ listEl.insertAdjacentHTML("beforeend", renderList);
+
+ const lightbox = new SimpleLightbox('.gallery a', { /* options */
+captionsData: "alt",
+captionDelay: 250,
+ });
