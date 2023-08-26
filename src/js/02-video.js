c7817ccd7c9ca634375d3c44db274d23acced1ac
GusarovaYuliya <93098363+GusarovaYuliya@users.noreply.github.com>
2023-08-26 03:35:50 +0300

    comm

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+// Ініціалізуй плеєр у файлі скрипта
+const iframe = document.querySelector('iframe');
+    const player = new Vimeo.Player(iframe);
+
+    player.on('play', function() {
+        console.log('played the video!');
+    });
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+// const player  = new Player("vimeo-player");
+
+// почни відстежувати подію timeupdate - оновлення часу відтворення.
+
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+// import
 
+// const feedbackForm = document.querySelector(".feedback-form");
+// const emailInput  = document.querySelector(".email");
+// const messageInput  = document.querySelector(".message");
+
+// Відстежуй на формі подію input, і щоразу записуй у локальне сховище об'єкт з полями email і message
+// const saveToLocalStorage =
+
+// у яких зберігай поточні значення полів форми
+// emailInput.addEventListener('input', saveToLocalStorage);
+// messageInput.addEventListener('input', saveToLocalStorage);
+
+// Нехай ключем для сховища буде рядок "feedback-form-state".
+// Під час завантаження сторінки перевіряй стан сховища, і якщо там є збережені дані, заповнюй ними поля форми. В іншому випадку поля повинні бути порожніми.
+// const savedData  = localStorage.getItem("feedback-form-state");
+// if(savedData) {
+//     const { email, message } = JSON.parse(savedData);
+
+    
+// }
+
+// Під час сабміту форми очищуй сховище і поля форми,
+
+// feedbackForm.addEventListener('submit', (event) => {
+//     event.preventDefault();
+//     localStorage.removeItem("feedback-form-state");
+//     emailInput.value = "";
+//     messageInput.value = "";
+// })
+
+
+//  а також виводь у консоль об'єкт з полями email, message та їхніми поточними значеннями.
+
+// console.log(emailInput.value);
+// console.log(messageInput.value)
